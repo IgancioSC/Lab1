812db77d9985d8c0a47ae302525cf761dc7423e2
Ignacio <ignacio.solisco@udlap.mx>
2024-02-15 13:06:50 -0600

    comand1

---
+++ ejercicios s1/trabajo.py
@@ -0,0 +1,4 @@
+horas = int(input("Introduce el número de horas trabajadas: "))
+costo = int(input("Introduce el costo por hora: "))
+paga=horas*costo
+print("El pago final a pagar es " + str(paga))
